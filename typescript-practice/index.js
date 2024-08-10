44cf9debfcb791edc253c7a649b2d7941ca9fac6
TakuyaArai6I20VV <leetgrow@gmail.com>
2024-08-10 14:51:03 +0900

    tsconfig

---
+++ typescript-practice/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let a = 2;
+console.log(a);
+let hello = "hello";
+console.log(hello.toUpperCase());
+//# sourceMappingURL=index.js.map
---
+++ typescript-practice/index.ts
@@ -0,0 +1,6 @@
+let a = 2;
+
+console.log(a);
+
+let hello = "hello";
+console.log(hello.toUpperCase());
