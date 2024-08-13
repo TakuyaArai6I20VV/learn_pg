4b8e104e8652da6b93e3538de28283789d8c861a
TakuyaArai6I20VV <leetgrow@gmail.com>
2024-08-13 17:08:37 +0900

    method

--- typescript-lesson/class.ts
+++ typescript-lesson/class.ts
@@ -6,9 +6,14 @@ class Person {
     this.name = initName;
     this.age = initAge;
   }
+
+  greeting(this: { name: string, age: number }) {
+    console.log(`Hello, ${this.name}!\nYour age is ${this.age}`);
+  }
 }
 
 const Mike = new Person('Mike', 22);
 console.log(Mike);
 console.log(Mike.name);
 console.log(Mike.age);
+Mike.greeting();
