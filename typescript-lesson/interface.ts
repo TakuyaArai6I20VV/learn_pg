8ffeecbfa1eaff06dd72e5a74bc15a5c2b53d996
TakuyaArai6I20VV <leetgrow@gmail.com>
2024-08-14 23:06:15 +0900

    implements

--- typescript-lesson/interface.ts
+++ typescript-lesson/interface.ts
@@ -1,9 +1,20 @@
 interface Human {
   name: string;
   age: number;
+  greeting(message: string): void;
 }
 
-const human: Human = {
-  name: 'John',
-  age: 22
+// const human: Human = {
+//   name: 'John',
+//   age: 22,
+//   greeting(message: string): void {
+//     console.log(message);
+//   }
+// }
+
+class Developer implements Human {
+  constructor(public name: string, public age: number) {}
+  greeting(message: string): void {
+    console.log(message);
+  }
 }
