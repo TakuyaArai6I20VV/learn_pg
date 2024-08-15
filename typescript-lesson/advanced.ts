dc2695911d4cfeda8fc035bfff5d5a9f1acfff83
TakuyaArai6I20VV <leetgrow@gmail.com>
2024-08-16 00:10:48 +0900

    type guard

--- typescript-lesson/advanced.ts
+++ typescript-lesson/advanced.ts
@@ -14,3 +14,42 @@ const mike: EngineerBlogger = {
   role: 'Software Engineer',
   follower: 2000
 }
+
+// Type guard
+function toUpperCase(x: string | number) {
+  if (typeof x === 'string') {
+    return x.toUpperCase();
+  }
+  return ''
+}
+type NomadWorker = Engineer | Blogger;
+function describeProfile(nomadWorker: NomadWorker) {
+  console.log(nomadWorker.name);
+  if ('role' in nomadWorker) {
+    console.log(nomadWorker.role);
+  }
+}
+
+// instance guard
+class Dog {
+  speak() {
+    console.log('Woof!');
+  }
+}
+
+class Bird {
+  speak() {
+    console.log('tweet-tweet');
+  }
+  fly() {
+    console.log('Flying');
+  }
+}
+type Pet = Dog | Bird;
+function havePet(pet: Pet) {
+  pet.speak();
+  if (pet instanceof Bird) {
+    pet.fly();
+  }
+}
+havePet(new Bird());
